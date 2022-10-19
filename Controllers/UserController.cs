c8b077452e57136c4821ed0b2d10e16d39b824ae
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2022-10-19 09:52:55 +0300

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,135 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using EntityLayer.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieProject.Models.ViewModels;
+
+namespace MovieProject.Controllers
+{
+    public class UserController : Controller
+    {
+        readonly UserManager<User> _userManager; //Kullanıcı Yönetimiyle ilgili her şeyi user manager sınıfı sayesinde yapıyoruz.
+
+        readonly IMapper _mapper;  //ViewModel'deki verileri gerçek modele aktarmamız gerekecektir işte bu durumda Automapper bu işi bizim için halledecek.
+
+        readonly SignInManager<User> _signInManager;
+
+        public UserController(UserManager<User> userManager, IMapper mapper, SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login(string ReturnUrl)
+        {
+            TempData["returnUrl"] = ReturnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User _user = await _userManager.FindByEmailAsync(model.Email);
+                if (_user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(_user, model.Password, model.Persistent, model.Lock);
+
+                    if (result.Succeeded)  //Kullanıcı Adı ve Şifre Doğru
+                    {
+
+                        await _userManager.ResetAccessFailedCountAsync(_user);
+
+                        if (string.IsNullOrEmpty(TempData["returnUrl"] != null ? TempData["returnUrl"].ToString() : ""))
+                            return RedirectToAction("Index");
+                        return Redirect(TempData["returnUrl"].ToString());
+                    }
+                    else
+                    {
+                        await _userManager.AccessFailedAsync(_user);
+
+                        int failcount = await _userManager.GetAccessFailedCountAsync(_user); //Kullanıcı'nın yapmış olduğu deneme adedini alıyoruz.
+
+                        if(failcount >= 3)
+                        {
+                            await _userManager.SetLockoutEndDateAsync(_user, new DateTimeOffset(DateTime.Now.AddMinutes(1)));
+                            ModelState.AddModelError("Locked", "Art arda 3 başarısız giriş denemesi yaptığınızdan dolayı hesabınız 1 dk kilitlenmiştir.");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("NotUser2", "E-Posta veya şifre yanlış");
+                        }
+                        
+                        ModelState.AddModelError("NotUser", "Böyle Bir Kullanıcı Bulunamadı");
+                        ModelState.AddModelError("NotUser2", "E-posta veya şifre yanlış.");
+                    }
+
+                }
+                else
+                {
+                    ModelState.AddModelError("NotUser", "Böyle Bir Kullanıcı Bulunamadı");
+                    ModelState.AddModelError("NotUser2", "E-posta veya şifre yanlış.");
+                }
+
+            }
+
+            return View(model);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserVM userVM)
+        {
+            if (ModelState.IsValid)
+            {
+                //User _User = new User
+                //{
+                //    UserName = userVM.UserName,
+                //    Email = userVM.Email
+                //};
+
+                User _User = _mapper.Map<User>(userVM);
+                
+                IdentityResult result = await _userManager.CreateAsync(_User, userVM.Password);  //Gerekli Kontroller Yapılıyor.
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index","Home");
+
+                else
+                {
+                    result.Errors.ToList().ForEach(e => ModelState.AddModelError(e.Code, e.Description));
+                }
+
+
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Login","User");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BusinessLayer.ValidationRules;
+using MovieProject.Models;
+using System;
+using FluentValidation.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+
+builder.Services.AddControllersWithViews().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<Program>());
+
+
+builder.Services.AddDbContext<MovieDBContext>(x => x.UseSqlServer("ConnectionStrings:SqlServerConnectionString"));
+//builder.Services.AddIdentity<User, UserRole>().AddEntityFrameworkStores<MovieDBContext>();
+
+
+
+builder.Services.AddIdentity<User, UserRole>(x =>
+{
+    x.Password.RequiredLength = 5; //En az kaç karakterli olmasý gerektiðini belirtiyoruz.
+    x.Password.RequireNonAlphanumeric = false; //Alfanumerik zorunluluðunu kaldýrýyoruz.
+    x.Password.RequireLowercase = false; //Küçük harf zorunluluðunu kaldýrýyoruz.
+    x.Password.RequireUppercase = false; //Büyük harf zorunluluðunu kaldýrýyoruz.
+    x.Password.RequireDigit = false; //0-9 arasý sayýsal karakter zorunluluðunu kaldýrýyoruz.
+
+    x.User.RequireUniqueEmail = true; //Email Adreslerini Tekilleþtiriyoruz.
+    x.User.AllowedUserNameCharacters = "abcçdefghiýjklmnoöpqrsþtuüvwxyzABCÇDEFGHIÝJKLMNOÖPQRSÞTUÜVWXYZ0123456789-._@+";
+
+}).AddPasswordValidator<CustomPasswordValidation>()          
+          .AddErrorDescriber<CustomUserValidation>().AddEntityFrameworkStores<MovieDBContext>();
+
+builder.Services.ConfigureApplicationCookie(x =>
+{
+    x.LoginPath = new PathString("/User/Login");
+    x.Cookie = new CookieBuilder
+    {
+        Name = "MovieProjectDB", //Oluþturulacak Cookie'yi isimlendiriyoruz.
+        HttpOnly = false, //Kötü niyetli insanlarýn client-side tarafýndan Cookie'ye eriþmesini engelliyoruz.
+        MaxAge = TimeSpan.FromMinutes(120), //Oluþturulacak Cookie'nin vadesini belirliyoruz.
+        SameSite = SameSiteMode.Lax, //Top level navigasyonlara sebep olmayan requestlere Cookie'nin gönderilmemesini belirtiyoruz.
+        SecurePolicy = CookieSecurePolicy.Always //HTTPS üzerinden eriþilebilir yapýyoruz.
+    };
+    x.SlidingExpiration = true; //Expiration süresinin yarýsý kadar süre zarfýnda istekte bulunulursa eðer geri kalan
+                                //yarýsýný tekrar sýfýrlayarak ilk ayarlanan süreyi tazeleyecektir.
+    x.ExpireTimeSpan = TimeSpan.FromMinutes(2); //CookieBuilder nesnesinde tanýmlanan Expiration deðerinin varsayýlan
+                                                //deðerlerle ezilme ihtimaline karþýn tekrardan Cookie vadesi burada da belirtiliyor.
+});
+
+//.AddPasswordValidator<CustomPasswordValidation>().AddEntityFrameworkStores<MovieDBContext>();
+//AddUserValidator<CustomUserValidation>()
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=User}/{action=SignUp}/{id?}");
+
+app.Run();
