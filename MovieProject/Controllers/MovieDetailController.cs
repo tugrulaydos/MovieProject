f7ada7a73d131ed990057597a750aebf762d06bc
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2022-10-21 15:46:49 +0300

    Home Page Tamamlandı

---
+++ MovieProject/Controllers/MovieDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieProject.Controllers
+{
+	public class MovieDetailController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
