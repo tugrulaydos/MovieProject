e2c54a7332e03256ec39b8549f4834a1989497df
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2022-11-04 11:50:36 +0300

    Some Modification is done and next is Ajax.

---
+++ EntityLayer/ViewModels/FilterVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.ViewModels
+{
+	public class FilterVM
+	{
+		public int GenreID{ get; set; }
+
+		public double ImdbMinValue { get; set; }
+
+		public double ImdbMaxValue { get; set; }
+
+		public short MinYear { get; set; }
+
+		public short MaxYear { get; set; }
+	}
+}
